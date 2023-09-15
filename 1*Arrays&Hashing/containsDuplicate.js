2992ddf075c36c568d19d72cd9043186a551174e
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-09-15 11:24:12 -0700

    folder structure

---
+++ 1*Arrays&Hashing/containsDuplicate.js
@@ -0,0 +1,22 @@
+
+//  @pram {number[]} 
+// @return {boolean}
+
+function containsDuplicate(nums) {
+   const setNum = new Set();
+   
+   for (const num of nums ) {
+     if (setNum.has(num)) {
+        return true; // Found a duplicate
+     }
+     setNum.add(num); // Add the number to the set
+   }
+
+   return false; //No Duplicate found 
+};
+//test case
+console.log(containsDuplicate([1,2,3,6,12,2,3]));
+console.log(containsDuplicate([123,2,3,6,12,2,3]))
+console.log(containsDuplicate([22,2,3,6,12,2,3]))
+
+//
