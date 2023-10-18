29bbda92a7321d8c95151dcd74fa1dc8a2a827f7
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-10-18 16:42:25 -0700

    test case modify

--- Js30/testF.js
+++ Js30/testF.js
@@ -1,39 +1,18 @@
-
-
-function expect(val) {
+var expect = function (val) {
     return {
-        toBe: function(otherVal) {
-            if(val !== otherVal){
-                throw new Error("Not Equal");
+        toBe: (value) => {
+            if (value === val) {
+                return true;
+            } else {
+                throw new Error('Not Equal');
             }
-            return {"value": true};
         },
-        notToBe: function(otherVal) {
-            if(val === otherVal) {
-                throw new Error("Equal");
+        notToBe: (value) => {
+            if (value !== val) {
+                return true;
+            } else {
+                throw new Error('Equal');
             }
-            return {"value": true};
         }
-    }
-}
-
-try{
-    const result1 = expect(5).toBe(5);
-    console.log(result1); //Output: {"value":true}
-} catch(error) {
-    console.log({"error": error.message})
-}
-
-try {
-    const result2 = expect(5).toBe(null);
-    console.log(result2);
-} catch(error) {
-    console.log({"error": error.message});
-}
-
-try {
-    const result3 = expect(5).notToBe(null);
-    console.log(result3); //
-} catch {
-    console.log({"error": error.message});
-}
+    };
+};
